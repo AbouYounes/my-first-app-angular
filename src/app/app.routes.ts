9a82d57e9a214a41cdf58ce05d2aff0018d6a0e4
AbouYounes <karim.khattou@univ-tiaret.dz>
2024-12-15 14:56:46 +0100

    404 page added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { AddComponent } from './add/add.component';
 import { ListComponent } from './list/list.component';
+import { NotfoundComponent } from './notfound/notfound.component';
 
 export const routes: Routes = [
     { path: '' , redirectTo: '/add' , pathMatch: 'full'},
     { path: 'add' , component: AddComponent},
     { path: 'list' , component: ListComponent},
+    { path: '**' , component: NotfoundComponent},
 ];
